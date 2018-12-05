4ac10536e32025f8d2d2c7491ef52d8b8059e581
Steven Crawford <scrawford@stsci.edu>
2018-12-04 19:39:28 -0700

    update directory searched for data and remove extra functions

--- specreduce/build_nist_table.py
+++ specreduce/build_nist_table.py
@@ -1,20 +1,21 @@
 """Build combined NIST table from txt files included in package
 """
 
-from astropy.table import Column, Table, vstack
 import glob
 import os
-import matplotlib.pyplot as plt
 import numpy as np
 import re
 
+from astropy.table import Column, Table, vstack
 
-def build_table():
+def build_table(line_lists=None):
     """Build master table from NIST txt files
 
     Parameters
     ----------
-    None
+    line_lists: list or None
+        A list of line table to read in.  If set to 'None',
+        it will read in tables in the NIST data directory
 
     Returns
     -------
@@ -23,7 +24,9 @@ def build_table():
     """
     names = ['Intensity', 'Wavelength', 'Element', 'Reference']
     # Use packaging directory instead of relative path in the future.
-    line_lists = glob.glob('data/line_lists/NIST/*.txt')
+    if line_lists is None:
+       code_dir = os.path.dirname(os.path.realpath(__file__))
+       line_lists = glob.glob(code_dir + '/data/line_lists/NIST/*.txt')
 
     tabs_to_stack = []
     for line_list in line_lists:
@@ -32,7 +35,7 @@ def build_table():
             tabs_to_stack.append(t)
         except:
             # Use numpy to parse table that arent comma delimited.
-            data = np.genfromtxt(line_list, 
+            data = np.genfromtxt(line_list,
                                 delimiter=(13, 14, 13, 16),
                                 dtype=str)
             t = Table(data, names=names,
@@ -41,22 +44,22 @@ def build_table():
 
     # Stack all of the tables.
     master_table = vstack(tabs_to_stack)
-    
+
     # Add on switch for users. Use line if True, don't if False
     # Set to True by default.
     on_off_column = Column([True] * len(master_table))
     master_table.add_column(on_off_column, name='On')
-    
+
     # Strip the numeric characters off of the intensities and add the letters
     # that denote intensities to their own column
     intensity = master_table['Intensity']
-    strength = [re.sub('[0-9]+', '', value).strip() for value in intensity]    
+    strength = [re.sub('[0-9]+', '', value).strip() for value in intensity]
     master_table.add_column(Column(strength), name='Strength')
 
     # Find and strip all alphabetic + special characters
     intensity_wo_strength = [re.sub('[a-zA-Z!@#$%^&*]', '', value).strip() \
                              for value in intensity]
-    
+
     # Delete old column
     master_table.remove_column('Intensity')
 
@@ -64,33 +67,10 @@ def build_table():
     master_table.add_column(Column(intensity_wo_strength, 
                                    dtype=int,
                                    name='Intensity'))
-    
+
     # Reorder table columns
     neworder = ('Element','Wavelength','Intensity', 'Strength', 'On', 'Reference')
     master_table = master_table[neworder]
 
     return master_table
 
-def save_table(location, table_name, table):
-    """Save parsed table to location
-
-    Parameters
-    ----------
-    location: str
-        Location you wish to save table to
-    table: astropytable
-
-    Returns
-    -------
-    None
-    """
-
-    table.write(os.path.join(location, table_name), format='csv')
-
-
-def main():
-    t = build_table()
-    save_table('data/line_lists/NIST/', 'NIST_combined.csv', t)
-
-if __name__ == "__main__":
-    main()
