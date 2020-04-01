8654783e166386abfd91ed126cfa97b269452980
T. E. Pickering <te.pickering@gmail.com>
2020-03-31 19:25:54 -0700

    update __init__.py to match new template

--- specreduce/__init__.py
+++ specreduce/__init__.py
@@ -1,18 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-"""
-This is an Astropy affiliated package.
-"""
-
-# Affiliated packages may add whatever they like to this file, but
+# Packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
-from ._astropy_init import *
+from ._astropy_init import *   # noqa
 # ----------------------------------------------------------------------------
-
-# For egg_info test builds to pass, put package imports here.
-if not _ASTROPY_SETUP_:
-    from .core import *
-
-__minimum_python_version__ = '3.6'
-__minimum_numpy_version__ = '1.10'
