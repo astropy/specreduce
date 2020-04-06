ee3844e0ff14687747fbef48b09ace3da364b97c
T. E. Pickering <te.pickering@gmail.com>
2020-04-06 16:01:18 -0700

    add ESO specphot standard star data

---
+++ specreduce/datasets/onedstds/eso/Xshooter/fix.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""
+the original data from the ESO ftp site does not include the 3rd column containing the bin width.
+this script recreates that using np.diff() on the wavelength axis and outputs an IRAF format
+file with columns for wavelength, AB mag, and bin width.
+"""
+
+import sys
+import numpy as np
+
+from astropy.table import Table
+
+
+infile = sys.argv[1]
+if infile[0] == 'm':
+    outfile = infile[1:]  # trim the leading 'm'
+else:
+    outfile = infile
+
+t = Table.read(infile, format='ascii')
+diff = np.diff(t['col1'].data)
+diff = np.append(diff, diff[-1])  # hack to add the extra binwidth at the end to make all arrays the same length
+
+t['diff'] = diff.round(3)
+
+t.write(outfile, format='ascii.fixed_width_no_header', delimiter='', overwrite=True)
