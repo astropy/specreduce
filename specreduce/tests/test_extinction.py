e566aefba066545fca13e72a181d26981cb03992
T. E. Pickering <te.pickering@gmail.com>
2020-04-23 22:13:40 -0700

    flesh out warnings; add methods of returning extiction as a linear transmission fraction or in magnitudes; add class to wrap IR transmission model that works like other extinction models; flesh out tests

--- specreduce/tests/test_extinction.py
+++ specreduce/tests/test_extinction.py
@@ -2,7 +2,11 @@ import numpy as np
 
 import astropy.units as u
 
-from ..calibration_data import AtmosphericExtinction, SUPPORTED_EXTINCTION_MODELS
+from ..calibration_data import (
+    AtmosphericExtinction,
+    AtmosphericIRExtinction,
+    SUPPORTED_EXTINCTION_MODELS
+)
 
 
 def test_supported_models():
@@ -11,14 +15,44 @@ def test_supported_models():
     """
     for model in SUPPORTED_EXTINCTION_MODELS:
         ext = AtmosphericExtinction(model=model)
-        assert(len(ext.extinction) > 0)
+        assert(len(ext.extinction_mag) > 0)
+        assert(len(ext.extinction_frac) > 0)
 
 
-def test_custom_model():
+def test_custom_mag_model():
+    """
+    Test creation of custom model from Quantity arrays
+    """
+    wave = np.linspace(0.3, 2.0, 50)
+    extinction = u.Magnitude(1. / wave, u.MagUnit(u.dimensionless_unscaled))
+    ext = AtmosphericExtinction(extinction=extinction, spectral_axis=wave * u.um)
+    assert(len(ext.extinction_mag) > 0)
+    assert(len(ext.extinction_frac) > 0)
+
+
+def test_custom_linear_model():
+    """
+    Test creation of custom model from Quantity arrays
+    """
+    wave = np.linspace(0.3, 2.0, 50)
+    extinction = 1. / wave * u.dimensionless_unscaled
+    ext = AtmosphericExtinction(extinction=extinction, spectral_axis=wave * u.um)
+    assert(len(ext.extinction_mag) > 0)
+    assert(len(ext.extinction_frac) > 0)
+
+
+def test_missing_extinction_unit():
     """
     Test creation of custom model from Quantity arrays
     """
     wave = np.linspace(0.3, 2.0, 50)
     extinction = 1. / wave
-    ext = AtmosphericExtinction(extinction=extinction * u.mag, spectral_axis=wave * u.um)
-    assert(len(ext.extinction) > 0)
+    ext = AtmosphericExtinction(extinction=extinction, spectral_axis=wave * u.um)
+    assert(len(ext.extinction_mag) > 0)
+    assert(len(ext.extinction_frac) > 0)
+
+
+def test_ir_extinction():
+    ext = AtmosphericIRExtinction()
+    assert(len(ext.extinction_mag) > 0)
+    assert(len(ext.extinction_frac) > 0)
