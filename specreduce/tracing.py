c85b8e15efa5a4d953f13444e943b634783c9ae7
T. E. Pickering <te.pickering@gmail.com>
2022-01-02 16:23:36 -0700

    refactor tracing classes into base class, class for flat traces, and class to handle input arrays

--- specreduce/tracing.py
+++ specreduce/tracing.py
@@ -5,42 +5,98 @@ from dataclasses import dataclass
 import numpy as np
 from astropy.nddata import CCDData
 
-__all__ = ['Trace']
+__all__ = ['Trace', 'FlatTrace', 'ArrayTrace']
 
 
 @dataclass
 class Trace:
     """
-    Basic tracing class that by default traces a constant horizontal
-    position, trace_pos, in the image.
+    Basic tracing class that by default traces the middle of the image.
 
     Parameters
     ----------
     image : `~astropy.nddata.CCDData`
         Image to be traced
-    trace_pos : float
-        Position of trace along vertical axis. If not specified, set to middle
-        of vertical axis
+
+    Properties
+    ----------
+    shape : tuple
+        Shape of the array describing the trace
     """
     image: CCDData
-    trace_pos: float = None
 
     def __post_init__(self):
-        if self.trace_pos is None:
-            self.trace_pos = self.image.shape[0] / 2
-        self.__call__(self.trace_pos)
+        self.trace_pos = self.image.shape[0] / 2
+        self.trace = np.ones_like(self.image[0]) * self.trace_pos
 
     def __getitem__(self, i):
         return self.trace[i]
 
-    def __call__(self, trace_pos):
-        """
-        Set vertical position of the trace and calculate the trace
-        """
-        self.trace_pos = trace_pos
-        self.trace = np.ones_like(self.image[0]) * self.trace_pos
-        return self.trace
-
     @property
     def shape(self):
         return self.trace.shape
+
+    def shift(self, delta):
+        """
+        Shift the trace by delta pixels perpendicular to the axis being traced
+
+        Parameters
+        ----------
+        delta : float
+            Shift to be applied to the trace
+        """
+        self.trace += delta
+        self._bound_trace()
+
+    def _bound_trace(self):
+        """
+        Set trace positions that are outside the bounds of the image to np.nan.
+        """
+        ny = self.image.shape[0]
+        self.trace = np.ma.masked_where(self.trace >= ny, self.trace)
+        self.trace = np.ma.masked_where(self.trace < 0, self.trace)
+
+
+@dataclass
+class FlatTrace(Trace):
+    """
+    Trace that is constant along the axis being traced
+
+    Parameters
+    ----------
+    trace_pos : float
+        Position of the trace
+    """
+    trace_pos: float
+
+    def __post_init__(self):
+        self.set_position(self.trace_pos)
+
+    def set_position(self, trace_pos):
+        """
+        Set the trace position within the image
+
+        Parameters
+        ----------
+        trace_pos : float
+            Position of the trace
+        """
+        self.trace_pos = trace_pos
+        self.trace = np.ones_like(self.image[0]) * self.trace_pos
+        self._bound_trace()
+
+
+@dataclass
+class ArrayTrace(Trace):
+    """
+    Define a trace given an array of trace positions
+
+    Parameters
+    ----------
+    trace : `numpy.ndarray`
+        Array containing trace positions
+    """
+    trace: np.ndarray
+
+    def __post_init__(self):
+        self._bound_trace()
