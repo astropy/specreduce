e2c7f9c6e22e2fdb09e605f308863293b390da00
Ivo <busko@stsci.edu>
2022-02-23 12:36:48 -0500

    Remove 3d code

--- specreduce/extract.py
+++ specreduce/extract.py
@@ -18,29 +18,19 @@ def _get_boxcar_weights(center, hwidth, npix):
     """
     weights = np.zeros((npix))
 
-    # 2d
-    if type(npix) is not tuple:
-        # pixels with full weight
-        fullpixels = [max(0, int(center - hwidth + 1)), min(int(center + hwidth), npix)]
-        weights[fullpixels[0]:fullpixels[1]] = 1.0
+    # pixels with full weight
+    fullpixels = [max(0, int(center - hwidth + 1)), min(int(center + hwidth), npix)]
+    weights[fullpixels[0]:fullpixels[1]] = 1.0
 
-        # pixels at the edges of the boxcar with partial weight
-        if fullpixels[0] > 0:
-            w = hwidth - (center - fullpixels[0] + 0.5)
-            if w >= 0:
-                weights[fullpixels[0] - 1] = w
-            else:
-                weights[fullpixels[0]] = 1. + w
-        if fullpixels[1] < npix:
-            weights[fullpixels[1]] = hwidth - (fullpixels[1] - center - 0.5)
-    # 3d
-    else:
-        # pixels with full weight
-        fullpixels_x = [max(0, int(center[1] - hwidth + 1)), min(int(center[1] + hwidth), npix[1])]
-        fullpixels_y = [max(0, int(center[0] - hwidth + 1)), min(int(center[0] + hwidth), npix[0])]
-        weights[fullpixels_x[0]:fullpixels_x[1], fullpixels_y[0]:fullpixels_y[1]] = 1.0
-
-        # not yet handling pixels at the edges of the boxcar
+    # pixels at the edges of the boxcar with partial weight
+    if fullpixels[0] > 0:
+        w = hwidth - (center - fullpixels[0] + 0.5)
+        if w >= 0:
+            weights[fullpixels[0] - 1] = w
+        else:
+            weights[fullpixels[0]] = 1. + w
+    if fullpixels[1] < npix:
+        weights[fullpixels[1]] = hwidth - (fullpixels[1] - center - 0.5)
 
     return weights
 
@@ -58,9 +48,9 @@ def _ap_weight_image(trace, width, disp_axis, crossdisp_axis, image_shape):
         width of extraction aperture in pixels
     disp_axis : int
         dispersion axis
-    crossdisp_axis : int (2D image) or tuple (3D image)
+    crossdisp_axis : int
         cross-dispersion axis
-    image_shape : tuple with 2 or 3 elements
+    image_shape : tuple with 2 elements
         size (shape) of image
 
     Returns
@@ -70,26 +60,12 @@ def _ap_weight_image(trace, width, disp_axis, crossdisp_axis, image_shape):
     """
     wimage = np.zeros(image_shape)
     hwidth = 0.5 * width
-
-    if len(crossdisp_axis) == 1:
-        # 2d
-        image_sizes = image_shape[crossdisp_axis[0]]
-    else:
-        # 3d
-        image_shape_array = np.array(image_shape)
-        crossdisp_axis_array = np.array(crossdisp_axis)
-        image_sizes = image_shape_array[crossdisp_axis_array]
-        image_sizes = tuple(image_sizes.tolist())
+    image_sizes = image_shape[crossdisp_axis[0]]
 
     # loop in dispersion direction and compute weights.
     for i in range(image_shape[disp_axis]):
-        if len(crossdisp_axis) == 1:
-            # 2d
-            # TODO trace must handle transposed data (disp_axis == 0)
-            wimage[:, i] = _get_boxcar_weights(trace[i], hwidth, image_sizes)
-        else:
-            # 3d
-            wimage[i, ::] = _get_boxcar_weights(trace[i], hwidth, image_sizes)
+        # TODO trace must handle transposed data (disp_axis == 0)
+        wimage[:, i] = _get_boxcar_weights(trace[i], hwidth, image_sizes)
 
     return wimage
 
@@ -112,8 +88,7 @@ class BoxcarExtract(SpecreduceOperation):
         The extracted 1d spectrum expressed in DN and pixel units
     """
     # TODO: what is a reasonable default?
-    # TODO: int or float?
-    width: int = 5
+    width: float = 5.
 
     # TODO: should disp_axis and crossdisp_axis be defined in the Trace object?
 
@@ -129,7 +104,7 @@ class BoxcarExtract(SpecreduceOperation):
             object with the trace
         disp_axis : int
             dispersion axis
-        crossdisp_axis : tuple (to support both 2D and 3D data)
+        crossdisp_axis : int
             cross-dispersion axis
 
 
@@ -146,7 +121,7 @@ class BoxcarExtract(SpecreduceOperation):
                 if getattr(self, attr) < 1:
                     raise ValueError(f'{attr} must be >= 1')
 
-        # images to use for extraction
+        # weight image to use for extraction
         wimage = _ap_weight_image(
             trace_object,
             self.width,
@@ -154,8 +129,7 @@ class BoxcarExtract(SpecreduceOperation):
             crossdisp_axis,
             image.shape)
 
-        # extract. Note that, for a cube, this is arbitrarily picking one of the
-        # spatial axis to collapse. This should be handled by the API somehow.
+        # extract
         ext1d = np.sum(image * wimage, axis=crossdisp_axis)
 
         # TODO: add wavelenght units, uncertainty and mask to spectrum1D object
