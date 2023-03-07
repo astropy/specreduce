a6119a92276f6571c348f5187722484377d610c3
T. E. Pickering <te.pickering@gmail.com>
2023-03-06 20:18:40 -0700

    fix docs and specphot tests

--- specreduce/tests/test_specphot_stds.py
+++ specreduce/tests/test_specphot_stds.py
@@ -6,19 +6,8 @@ from ..calibration_data import (
 )
 
 
-def test_load_MAST_remote():
-    sp = load_MAST_calspec("g191b2b_005.fits", remote=True, show_progress=False)
-    assert sp is not None
-    assert len(sp.spectral_axis) > 0
-
-
-def test_load_MAST_local():
-    sp_file = download_file(
-        "https://archive.stsci.edu/hlsps/reference-atlases/cdbs/calspec/g191b2b_005.fits",
-        show_progress=False,
-        pkgname='specreduce'
-    )
-    sp = load_MAST_calspec(sp_file, remote=False)
+def test_load_MAST():
+    sp = load_MAST_calspec("g191b2b_005.fits", show_progress=False)
     assert sp is not None
     assert len(sp.spectral_axis) > 0
 
