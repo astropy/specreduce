16a4060e347d9890850485fd3bca1083172898b2
T. E. Pickering <te.pickering@gmail.com>
2023-03-09 19:32:45 -0700

    initial implementation to make a synthetic 2D arc lamp image

--- specreduce/utils/synth_data.py
+++ specreduce/utils/synth_data.py
@@ -7,6 +7,10 @@ from photutils.datasets import apply_poisson_noise
 import astropy.units as u
 from astropy.modeling import models
 from astropy.nddata import CCDData
+from astropy.wcs import WCS
+from astropy.stats import gaussian_fwhm_to_sigma
+
+from specreduce.calibration_data import load_pypeit_calibration_lines
 
 
 def make_2dspec_image(
@@ -66,5 +70,70 @@ def make_2dspec_image(
     noisy_image = apply_poisson_noise(z)
 
     ccd_im = CCDData(noisy_image, unit=u.count)
+
+    return ccd_im
+
+
+def make_2d_arc_image(
+    nx=3000,
+    ny=1000,
+    extent=[3500, 7000],
+    wcs=None,
+    wave_unit=u.Angstrom,
+    background=5,
+    line_fwhm=5.,
+    linelists=['HeI'],
+    amplitude_scale=1.
+):
+    if wcs is None:
+        if extent is None:
+            raise ValueError("Must specify either a wavelength extent or a WCS.")
+        if len(extent) != 2:
+            raise ValueError("Wavelength extent must be of length 2.")
+        wcs = WCS(naxis=2)
+        wcs.wcs.ctype[0] = 'WAVE'
+        wcs.wcs.ctype[1] = 'PIXEL'
+        wcs.wcs.cunit[0] = wave_unit
+        wcs.wcs.cunit[1] = u.pixel
+        wcs.wcs.crval[0] = extent[0]
+        wcs.wcs.cdelt[0] = (extent[1] - extent[0]) / nx
+        wcs.wcs.crval[1] = 0
+        wcs.wcs.cdelt[1] = 1
+    else:
+        if not wcs.has_spectral:
+            raise ValueError("Provided WCS must have a spectral axis.")
+        if wcs.naxis != 2:
+            raise ValueError("WCS must have NAXIS=2 for a 2D image.")
+
+    x = np.arange(nx)
+    y = np.arange(ny)
+    xx, yy = np.meshgrid(x, y)
+
+    is_spectral = [a['coordinate_type'] == "spectral" for a in wcs.get_axis_types()]
+    if is_spectral[0]:
+        disp_axis = 0
+    else:
+        disp_axis = 1
+
+    linelist = load_pypeit_calibration_lines(linelists)
+    line_disp_positions = wcs.spectral.world_to_pixel(linelist['wave'])
+
+    z = background + np.zeros((ny, nx))
+
+    line_sigma = gaussian_fwhm_to_sigma * line_fwhm
+    for line_pos, ampl in zip(line_disp_positions, linelist['amplitude']):
+        line_mod = models.Gaussian1D(
+            amplitude=ampl * amplitude_scale,
+            mean=line_pos,
+            stddev=line_sigma
+        )
+        if disp_axis == 0:
+            z += line_mod(xx)
+        else:
+            z += line_mod(yy)
+
+    noisy_image = apply_poisson_noise(z)
+
+    ccd_im = CCDData(noisy_image, unit=u.count, wcs=wcs)
 
     return ccd_im
