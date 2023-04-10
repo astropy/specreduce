7f31b61fe991838530e19c3e0b93643ae8ec00d2
Ricky O'Steen <rosteen@stsci.edu>
2023-04-10 15:52:20 -0400

    Increase test coverage

--- specreduce/tests/test_wavelength_calibration.py
+++ specreduce/tests/test_wavelength_calibration.py
@@ -1,8 +1,10 @@
 from numpy.testing import assert_allclose
+import pytest
 
 from astropy.table import QTable
 import astropy.units as u
 from astropy.modeling.models import Polynomial1D
+from astropy.modeling.fitting import LinearLSQFitter
 from astropy.tests.helper import assert_quantity_allclose
 
 from specreduce import WavelengthCalibration1D
@@ -16,6 +18,13 @@ def test_linear_from_list(spec1d):
 
     assert_quantity_allclose(spec2.spectral_axis[0], 4998.8*u.AA)
     assert_quantity_allclose(spec2.spectral_axis[-1], 5495.169999*u.AA)
+
+
+def test_wavelength_from_table(spec1d):
+    centers = [0, 10, 20, 30]
+    w = [5000, 5100, 5198, 5305]*u.AA
+    table = QTable([w], names=["wavelength"])
+    WavelengthCalibration1D(spec1d, centers, line_wavelengths=table)
 
 
 def test_linear_from_table(spec1d):
@@ -35,7 +44,7 @@ def test_poly_from_table(spec1d):
     w = [5005, 5110, 5214, 5330, 5438]*u.AA
     table = QTable([centers, w], names=["pixel_center", "wavelength"])
 
-    test = WavelengthCalibration1D(spec1d, table, model=Polynomial1D(2))
+    test = WavelengthCalibration1D(spec1d, table, model=Polynomial1D(2), fitter=LinearLSQFitter())
     test.apply_to_spectrum(spec1d)
 
     assert_allclose(test.model.parameters, [5.00477143e+03, 1.03457143e+01, 1.28571429e-02])
@@ -52,3 +61,23 @@ def test_replace_spectrum(spec1d, spec1d_with_emission_line):
     # Replace the input spectrum, which should clear the cached properties
     test.input_spectrum = spec1d_with_emission_line
     assert "wcs" not in test.__dict__
+
+
+def test_expected_errors(spec1d):
+    centers = [0, 10, 20, 30, 40]
+    w = [5005, 5110, 5214, 5330, 5438]*u.AA
+    table = QTable([centers, w], names=["pixel_center", "wavelength"])
+
+    with pytest.raises(ValueError, match="Cannot specify line_wavelengths separately"):
+        WavelengthCalibration1D(spec1d, table, line_wavelengths=w)
+
+    with pytest.raises(ValueError, match="must have the same length"):
+        w2 = [5005, 5110, 5214, 5330, 5438, 5500]*u.AA
+        WavelengthCalibration1D(spec1d, table, line_wavelengths=w2)
+
+    with pytest.raises(ValueError, match="must have the same length"):
+        w2 = [5005, 5110, 5214, 5330, 5438, 5500]*u.AA
+        WavelengthCalibration1D(spec1d, table, line_wavelengths=w2)
+
+    with pytest.raises(ValueError, match="specify at least one"):
+        WavelengthCalibration1D(spec1d, centers)
