d035f85558f333a84e52348b0d1550bdc011eb83
T. E. Pickering <te.pickering@gmail.com>
2023-05-10 18:11:20 -0700

    remove check for astropy < 3.x

--- specreduce/conftest.py
+++ specreduce/conftest.py
@@ -5,23 +5,17 @@
 
 import os
 
-from astropy.version import version as astropy_version
 import astropy.units as u
 import numpy as np
 import pytest
 from specutils import Spectrum1D
 
-# For Astropy 3.0 and later, we can use the standalone pytest plugin
-if astropy_version < '3.0':
-    from astropy.tests.pytest_plugins import *  # noqa
-    del pytest_report_header
+
+try:
+    from pytest_astropy_header.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
     ASTROPY_HEADER = True
-else:
-    try:
-        from pytest_astropy_header.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
-        ASTROPY_HEADER = True
-    except ImportError:
-        ASTROPY_HEADER = False
+except ImportError:
+    ASTROPY_HEADER = False
 
 
 @pytest.fixture
