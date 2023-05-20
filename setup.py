6f1a7aa8efca2e665592a35e49c9afd87bbf51f8
T. E. Pickering <te.pickering@gmail.com>
2023-05-19 17:57:41 -0700

    remove astropy_helpers vestige; packaging cleanups; set python_requires to >= 3.10

--- setup.py
+++ setup.py
@@ -9,9 +9,6 @@ import sys
 
 from setuptools import setup
 
-from extension_helpers import get_extensions
-
-
 # First provide helpful messages if contributors try and run legacy commands
 # for tests or docs.
 
@@ -65,5 +62,20 @@ if 'build_docs' in sys.argv or 'build_sphinx' in sys.argv:
     print(DOCS_HELP)
     sys.exit(1)
 
-setup(use_scm_version={'write_to': os.path.join('specreduce', 'version.py')},
-      ext_modules=get_extensions())
+VERSION_TEMPLATE = """
+# Note that we need to fall back to the hard-coded version if either
+# setuptools_scm can't be imported or setuptools_scm can't determine the
+# version, so we catch the generic 'Exception'.
+try:
+    from setuptools_scm import get_version
+    version = get_version(root='..', relative_to=__file__)
+except Exception:
+    version = '{version}'
+""".lstrip()
+
+setup(
+    use_scm_version={
+        'write_to': os.path.join('specreduce', 'version.py'),
+        'write_to_template': VERSION_TEMPLATE
+    }
+)
