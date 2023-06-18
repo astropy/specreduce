9d27c7e73427c2c67718a4872c3ebe419adf19da
T. E. Pickering <te.pickering@gmail.com>
2023-06-18 23:12:19 +0200

    add routine to find arc lines and centroid them

--- specreduce/line_matching.py
+++ specreduce/line_matching.py
@@ -3,9 +3,71 @@ from typing import Sequence
 import numpy as np
 
 import astropy.units as u
+from astropy.stats import gaussian_fwhm_to_sigma, gaussian_sigma_to_fwhm
+from astropy.modeling import models
 from astropy.table import QTable
 from astropy.wcs import WCS as astropy_WCS
+
 from gwcs.wcs import WCS as gWCS
+
+from specutils import Spectrum1D
+from specutils.fitting import find_lines_threshold, fit_lines
+
+
+__all__ = [
+    "find_arc_lines",
+    "match_lines_wcs"
+]
+
+
+def find_arc_lines(
+    spectrum: Spectrum1D,
+    fwhm: u.Quantity = 5.0 * u.pix,
+    window: float = 3.0,
+    noise_factor: float = 5.0
+) -> QTable:
+    """
+    Find arc lines in a spectrum using `~specutils.fitting.find_lines_threshold` and
+    then perform gaussian fits to each detected line to refine position and FWHM.
+
+    Parameters
+    ----------
+    spectrum : The extracted arc spectrum to search for lines. It should be background-subtracted
+        and must have an "uncertainty" attribute.
+
+    fwhm : Estimated full-width half-maximum of the lines in pixels.
+
+    window : The window size in units of fwhm to use for the gaussian fits.
+
+    noise_factor : The factor to multiply the uncertainty by to determine the noise threshold
+        in the `~specutils.fitting.find_lines_threshold` routine.
+
+    Returns
+    -------
+    QTable
+        A table of detected arc lines and their properties.
+    """
+    detected_lines = find_lines_threshold(spectrum, noise_factor=noise_factor)
+    detected_lines = detected_lines[detected_lines['line_type'] == 'emission']
+
+    centroids = []
+    widths = []
+    amplitudes = []
+    for r in detected_lines:
+        g_init = models.Gaussian1D(
+            amplitude=spectrum.flux[r['line_center_index']],
+            mean=r['line_center'],
+            stddev=fwhm * gaussian_fwhm_to_sigma
+        )
+        g_fit = fit_lines(spectrum, g_init, window=window * fwhm)
+        centroids.append(g_fit.mean.value)
+        widths.append(g_fit.stddev * gaussian_sigma_to_fwhm)
+        amplitudes.append(g_fit.amplitude.value)
+    line_table = QTable()
+    line_table['centroid'] = centroids
+    line_table['fwhm'] = widths
+    line_table['amplitude'] = amplitudes
+    return line_table
 
 
 def match_lines_wcs(
