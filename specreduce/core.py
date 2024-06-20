fix issue with units (#221)
* fix issue with units

* .

* add section for 1.4.1

specreduce/core.py
@@ -51,7 +51,21 @@ class _ImageParser:
             # useful for Background's instance methods
             return self.image
 
-        img = self._get_data_from_image(image)
+        img = self._get_data_from_image(image, disp_axis=disp_axis)
+
+        return img
+
+    @staticmethod
+    def _get_data_from_image(image, disp_axis=1):
+        """Extract data array from various input types for `image`.
+           Retruns `np.ndarray` of image data."""
+
+        if isinstance(image, u.quantity.Quantity):
+            img = image.value
+        elif isinstance(image, np.ndarray):
+            img = image
+        else:  # NDData, including CCDData and Spectrum1D
+            img = image.data
 
         # mask and uncertainty are set as None when they aren't specified upon
         # creating a Spectrum1D object, so we must check whether these
@@ -74,17 +88,6 @@ class _ImageParser:
         return Spectrum1D(img * unit, spectral_axis=spectral_axis,
                           uncertainty=uncertainty, mask=mask)
 
-    @staticmethod
-    def _get_data_from_image(image):
-        """Extract data array from various input types for `image`.
-           Retruns `np.ndarray` of image data."""
-
-        if isinstance(image, u.quantity.Quantity):
-            img = image.value
-        if isinstance(image, np.ndarray):
-            img = image
-        else:  # NDData, including CCDData and Spectrum1D
-            img = image.data
         return img
 
 