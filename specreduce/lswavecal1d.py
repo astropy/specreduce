0e29e3d442b949ebd3808863007749dbbd3aaa28
Hannu Parviainen <hannu@iac.es>
2025-01-15 14:23:18 -0500

    Renamed and refactored the 1D wavelength calibration module.
    Renamed `wavecal.py` to `lswavecal1d.py` and refactored the `WavelengthSolution1D` class for improved clarity and functionality. Added new methods like `refine_fit`, `pix_to_wav`, and `wav_to_pix`, and modified existing ones to enhance flexibility and maintainability.

--- specreduce/lswavecal1d.py
+++ specreduce/lswavecal1d.py
@@ -21,15 +21,6 @@ def diff_poly1d(m: models.Polynomial1D) -> models.Polynomial1D:
     return models.Polynomial1D(m.degree-1, **coeffs)
 
 
-def diff_poly2d_x(m):
-    coeffs = {}
-    for n in m.param_names:
-        ix, iy = int(n[1]), int(n[3])
-        if ix > 0:
-            coeffs[f"c{ix-1}_{iy}"] = ix*getattr(m, n).value
-    return models.Polynomial2D(m.degree-1, **coeffs)
-
-
 class WavelengthSolution1D:
     def __init__(self, arc_spectra, lamps, wlbounds: tuple[float, float], wave_air: bool = False):
         self.arc_spectra = arc_spectra
@@ -42,12 +33,13 @@ class WavelengthSolution1D:
         self._fitted_model: Model | None = None
         self._fit: optimize.OptimizeResult | None = None
         self._wcs: wcs.WCS | None = None
-        self._read_linelists()
-
+        self._trees: Iterable[KDTree] | None = None
         self._p2w: Model | None = None           # The fitted pixel -> wavelength model
         self._w2p: Model | None = None           # The fitted wavelength -> pixel model
         self._p2w_dldx: Model | None = None      # delta lambda / delta pixel
         self._w2p_dxdl: Model | None = None      # delta pixel / delta lambda
+
+        self._read_linelists()
 
     def _read_linelists(self):
         for l in self.lamps:
@@ -55,40 +47,47 @@ class WavelengthSolution1D:
             self.linelists.append(ll[(ll['wavelength'].value > self.wlbounds[0]) &
                                      (ll['wavelength'].value < self.wlbounds[1])])
         self.lines_wav = [lo['wavelength'].value for lo in self.linelists]
+        self._trees = [KDTree(l[:, None]) for l in self.lines_wav]
 
-    def find_lines(self):
+    def find_lines(self, fwhm: float):
         with warnings.catch_warnings():
             warnings.simplefilter('ignore')
-            lines_obs = [find_arc_lines(sp, 5) for sp in self.arc_spectra]
+            lines_obs = [find_arc_lines(sp, fwhm) for sp in self.arc_spectra]
         self.lines_pix = [lo['centroid'].value for lo in lines_obs]
 
-    def fit(self, ref_pixel: float, wl0: tuple[float, float] = (7000, 7600), dwl: tuple[float, float] = (2.4, 2.8),
+    def fit(self, ref_pixel: float, wl0: tuple[float, float], dwl: tuple[float, float],
             popsize: int = 30, max_distance: float = 100):
 
         model = models.Shift(-ref_pixel) | models.Polynomial1D(3)
-        trees = [KDTree(l[:, None]) for l in self.lines_wav]
-
         def minfun(x):
             return sum(
                 [np.clip(t.query(model.evaluate(l, -ref_pixel, *x)[:, None])[0], 0, max_distance).sum() for t, l in
-                 zip(trees, self.lines_pix)])
+                 zip(self._trees, self.lines_pix)])
 
         # Calculate the pixel -> wavelength transform and its derivative along the spectral axis
         self._fit = fit = optimize.differential_evolution(minfun,
                                                           bounds=[wl0, dwl, [-1e-3, 1e-3], [-1e-4, 1e-4]],
                                                           popsize=popsize)
-        self._p2w = p2w = models.Shift(-ref_pixel) | models.Polynomial1D(3, **{f'c{i}': fit.x[i] for i in
+        self._p2w = models.Shift(-ref_pixel) | models.Polynomial1D(3, **{f'c{i}': fit.x[i] for i in
                                                                                   range(fit.x.size)})
+        self.refine_fit()
+        p2w = self._p2w
         self._p2w_dldx = models.Shift(p2w.offset_0) | diff_poly1d(p2w[1])
 
         # Calculate the wavelength -> pixel transform and its derivative along the spectral axis
         vpix = self.arc_spectra[0].spectral_axis.value
         vwav = p2w(vpix)
-        w2p = models.Polynomial1D(7, c0=-p2w.offset_0, c1=1 / p2w.c1_1, fixed={'c0': True})
+        w2p = models.Polynomial1D(6, c0=-p2w.offset_0, c1=1 / p2w.c1_1, fixed={'c0': True})
         with warnings.catch_warnings():
             warnings.simplefilter('ignore')
-            self._w2p = w2p = models.Shift(-p2w.c0_1) | fitting.LMLSQFitter()(w2p, vwav - p2w.c0_1.value, vpix)
+            self._w2p = w2p = models.Shift(-p2w.c0_1) | fitting.DogBoxLSQFitter()(w2p, vwav - p2w.c0_1.value, vpix)
         self._w2p_dxdl = models.Shift(w2p.offset_0) | diff_poly1d(w2p[1])
+
+    def refine_fit(self, degree: int = 4, match_distance_bound: float = 5.0):
+        matched_pix, matched_wav = self.match_lines(match_distance_bound)
+        model = models.Polynomial1D(degree, **{n: getattr(self._p2w[-1], n).value for n in self._p2w[-1].param_names})
+        fitter = fitting.LinearLSQFitter()
+        self._p2w = self._p2w[0].copy() | fitter(model, matched_pix + self._p2w.offset_0.value, matched_wav)
 
     def resample(self, flux, nbins: int | None = None, wlbounds: tuple[float, float] | None = None,
                  bin_edges: Iterable[float] | None = None):
@@ -123,6 +122,12 @@ class WavelengthSolution1D:
         flux_wl *= n
         return bin_centers_wav, flux_wl
 
+    def pix_to_wav(self, pix: ndarray |float) -> ndarray | float:
+        return self._p2w(pix)
+
+    def wav_to_pix(self, wav: ndarray | float) -> ndarray | float:
+        return self._w2p(wav)
+
     @property
     def wcs(self):
         pixel_frame = cf.CoordinateFrame(1, "SPECTRAL", [0, ], axes_names=["x", ], unit=[u.pix])
@@ -135,37 +140,49 @@ class WavelengthSolution1D:
     def fitted_model(self):
         return self._p2w
 
+    def match_lines(self, upper_bound: float = 5):
+        matched_lines_wav = []
+        matched_lines_pix = []
+        for iframe, tree in enumerate(self._trees):
+            l, ix = tree.query(self._p2w(self.lines_pix[iframe])[:, None], distance_upper_bound=upper_bound)
+            m = np.isfinite(l)
+            matched_lines_wav.append(tree.data[ix[m], 0])
+            matched_lines_pix.append(self.lines_pix[iframe][m])
+        return np.concatenate(matched_lines_pix), np.concatenate(matched_lines_wav)
+
     def plot_lines(self, axes=None, figsize=None):
         if axes is None:
-            fig, axes = subplots(len(self.arc_spectra), 1, figsize=figsize, sharex='all', constrained_layout=True)
+            fig, axes = subplots(len(self.arc_spectra), 1, figsize=figsize, sharex='all', constrained_layout=True, squeeze=False)
         else:
             fig = axes[0].figure
         for i, sp in enumerate(self.arc_spectra):
-            axes[i].plot(sp.data / (1.2 * sp.data.max()))
-            axes[i].vlines(self.lines_pix[i], 0.0, 1, alpha=0.1)
-            axes[i].vlines(self.lines_pix[i], 0.9, 1)
-            axes[i].autoscale(enable=True, axis='x', tight=True)
+            axes[i,0].plot(sp.data / (1.2 * sp.data.max()))
+            axes[i,0].vlines(self.lines_pix[i], 0.0, 1, alpha=0.1)
+            axes[i,0].vlines(self.lines_pix[i], 0.9, 1)
+            axes[i,0].autoscale(enable=True, axis='x', tight=True)
         setp(axes[-1], xlabel='Pixel')
         return fig
 
-    def plot_solution(self, axes=None, figsize=None):
+    def plot_solution(self, axes=None, figsize=None, model = None):
         if axes is None:
-            fig, axes = subplots(len(self.arc_spectra), 1, figsize=figsize, sharex='all', constrained_layout=True)
+            fig, axes = subplots(len(self.arc_spectra), 1, figsize=figsize, sharex='all', constrained_layout=True, squeeze=False)
         else:
             fig = axes[0].figure
 
+        model = model if model is not None else self.fitted_model
+
         for i, sp in enumerate(self.arc_spectra):
-            axes[i].plot(self.fitted_model(sp.spectral_axis.value), sp.data / (1.2 * sp.data.max()))
-            axes[i].vlines(self.lines_wav[i], 0.0, 1.0, alpha=0.3, ec='darkorange', zorder=0)
-            axes[i].vlines(self.fitted_model(self.lines_pix[i]), 0.9, 1.0, alpha=1)
-            axes[i].autoscale(enable=True, axis='x', tight=True)
+            axes[i,0].plot(model(sp.spectral_axis.value), sp.data / (1.2 * sp.data.max()))
+            axes[i,0].vlines(self.lines_wav[i], 0.0, 1.0, alpha=0.3, ec='darkorange', zorder=0)
+            axes[i,0].vlines(model(self.lines_pix[i]), 0.9, 1.0, alpha=1)
+            axes[i,0].autoscale(enable=True, axis='x', tight=True)
         setp(axes[-1], xlabel=f'Wavelength [{self.linelists[0]["wavelength"].unit.to_string(format="latex")}]')
         return fig
 
     def plot_transforms(self, figsize=None):
         fig, axs = subplots(2, 2, figsize=figsize, constrained_layout=True, sharex='col')
         xpix = self.arc_spectra[0].spectral_axis.value
-        xwav = np.linspace(5000, 11000, 2000)
+        xwav = np.linspace(*self.pix_to_wav(xpix[[0, -1]]), num=xpix.size)
         axs[0, 0].plot(xpix, self._p2w(xpix), 'k')
         axs[1, 0].plot(xpix[:-1], np.diff(self._p2w(xpix)) / np.diff(xpix), lw=4, c='k')
         axs[1, 0].plot(xpix, self._p2w_dldx(xpix), ls='--', lw=2, c='w')
