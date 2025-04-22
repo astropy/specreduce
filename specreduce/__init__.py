ba8c433d438cc1fd6bd4181e57e6066390964912
Hannu Parviainen <hpparvi@gmail.com>
2025-04-22 20:33:14 +0100

    removed a reference to old wavelength_calibration module from the main __init__.py

--- specreduce/__init__.py
+++ specreduce/__init__.py
@@ -1,8 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 from specreduce.core import *  # noqa
-from specreduce.wavelength_calibration import * # noqa
-
 
 try:
     from .version import version as __version__
