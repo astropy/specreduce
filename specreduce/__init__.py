3034da5372ae77adf931904753cf3564ebcb3f37
Hannu Parviainen <hpparvi@gmail.com>
2025-04-24 12:36:38 +0100

    Brought back the original wavelength_calibration.py code so that we don't break backwards compatibility (quite yet).

--- specreduce/__init__.py
+++ specreduce/__init__.py
@@ -1,6 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 from specreduce.core import *  # noqa
+from specreduce.wavelength_calibration import * # noqa
 
 try:
     from .version import version as __version__
